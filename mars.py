fe0f331549d183f514223f239a774531228e0118
Kenti <matniyazov@inbox.ru>
2024-02-10 19:56:21 +0400

    n3

--- mars.py
+++ mars.py
@@ -8,13 +8,23 @@ app = Flask(__name__)
 def index():
     param = {}
     param['title'] = 'Заготовка'
-    param['spec'] = ''
     return render_template('base.html', **param)
 @app.route('/training/<prof>')
 def training(prof):
     param = {}
     param['prof'] = prof
     return render_template('train.html', **param)
+
+@app.route('/list_prof/<list1>')
+def  list_prof(list1):
+    param = {}
+    param['title'] = 'Заготовка'
+    param['list'] = list1
+    param['profs'] = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                      'инженер по терраформированию', 'климатолог', 'специалист по радиационной защите', 'астрогеолог',
+                      'гляциолог', 'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                      'штурман', 'пилот дронов']
+    return render_template('proffesions.html', **param)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
