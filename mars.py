778f4f5e1294b2e0aa0e403522da16d0e6e13c5a
Kenti <matniyazov@inbox.ru>
2024-02-10 19:29:52 +0400

    n2

--- mars.py
+++ mars.py
@@ -8,9 +8,13 @@ app = Flask(__name__)
 def index():
     param = {}
     param['title'] = 'Заготовка'
+    param['spec'] = ''
     return render_template('base.html', **param)
-
-
+@app.route('/training/<prof>')
+def training(prof):
+    param = {}
+    param['prof'] = prof
+    return render_template('train.html', **param)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
